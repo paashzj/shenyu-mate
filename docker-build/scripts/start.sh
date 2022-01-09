1c0526ab893dfc81a401b54eb2b727523d4fbd2c
lovehzj <1922919664@qq.com>
2022-01-09 16:01:29 +0800

    add start daemon script

--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir /opt/sh/shenyu/logs
-
-java -Xmx128M -Xms128M -XX:MaxDirectMemorySize=32M -jar /opt/sh/shenyu/mate/shenyu-mate.jar >/opt/sh/shenyu/logs/shenyu_mate.stdout.log 2>/opt/sh/shenyu/logs/shenyu_mate.stderr.log
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+bash -x $DIR/start-daemon.sh
 tail -f /dev/null
