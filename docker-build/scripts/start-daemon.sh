6d00cb5ac8ba6509e2c219b3133884e9a9a88d5d
lovehzj <1922919664@qq.com>
2022-01-13 23:08:27 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $SHENYU_HOME/logs
+mkdir -p $SHENYU_HOME/logs
 
 java -Xmx128M -Xms128M -XX:MaxDirectMemorySize=32M -jar $SHENYU_HOME/mate/shenyu-mate.jar >>$SHENYU_HOME/logs/shenyu_mate.stdout.log 2>>$SHENYU_HOME/logs/shenyu_mate.stderr.log
